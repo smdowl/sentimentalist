31ea696cf71c7a9a099a439186af13a2e339d7b9
Shaun Dowling <shaun@duedil.com>
2014-03-28 22:30:02 +0000

    Added a suggestion for the way we can store additional data after preprocessing.

---
+++ src/main/java/com/whereismydot/dataobjects/AugStatus.java
@@ -0,0 +1,41 @@
+package com.whereismydot.dataobjects;
+
+import com.google.gson.Gson;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonParser;
+import com.google.gson.stream.JsonReader;
+import twitter4j.Status;
+import twitter4j.TwitterException;
+import twitter4j.TwitterObjectFactory;
+
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.Reader;
+
+public class AugStatus {
+    private static JsonParser parser = new JsonParser();
+    private static Gson gson = new Gson();
+
+    public Status tweet;
+
+    public AugStatus(Reader reader) throws TwitterException {
+        JsonReader jsonReader = new JsonReader(reader);
+
+        JsonElement elem =  parser.parse(jsonReader);
+        String cleanJson = gson.toJson(elem);
+
+        tweet = TwitterObjectFactory.createStatus(cleanJson);
+
+    }
+
+    public String toString() {
+        return tweet.toString();
+    }
+
+    public static void main(String[] args) throws TwitterException {
+        InputStream in = AugStatus.class.getResourceAsStream("/example_tweet.json");
+        AugStatus status = new AugStatus(new InputStreamReader(in));
+
+        System.out.println(status);
+    }
+}
