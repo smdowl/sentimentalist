9e4d4d1c5a64f3155d0496a1c39c978bb373acc2
Shaun Dowling <shaun@duedil.com>
2014-03-30 20:22:09 +0100

    Made access to the example tweet a public method.

--- src/main/java/experimentation/TwitterTest.java
+++ src/main/java/experimentation/TwitterTest.java
@@ -5,6 +5,7 @@ import com.google.gson.JsonElement;
 import com.google.gson.JsonParser;
 import com.google.gson.stream.JsonReader;
 import twitter4j.Status;
+import twitter4j.TwitterException;
 import twitter4j.TwitterObjectFactory;
 
 import java.io.InputStream;
@@ -13,7 +14,11 @@ import java.io.InputStreamReader;
 public class TwitterTest {
 
     public static void main(String[] args) throws Exception {
+        Status tweet = getExampleTweet();
+        System.out.println(tweet);
+    }
 
+    public static Status getExampleTweet() throws TwitterException {
         JsonParser parser = new JsonParser();
         Gson gson = new Gson();
 
@@ -23,7 +28,7 @@ public class TwitterTest {
         JsonElement elem =  parser.parse(reader);
         String cleanJson = gson.toJson(elem);
 
-        Status tweet = TwitterObjectFactory.createStatus(cleanJson);
-        System.out.println(tweet);
+        return TwitterObjectFactory.createStatus(cleanJson);
+
     }
 }
