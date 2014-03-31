d1c11553f4ffb21e33d75280edd809e1dd2d8233
Shaun Dowling <shaun@duedil.com>
2014-03-31 21:55:57 +0100

    Updated classifier to only compare lower case strings.

--- src/main/java/com/whereismydot/utils/CompanyClassifier.java
+++ src/main/java/com/whereismydot/utils/CompanyClassifier.java
@@ -25,21 +25,23 @@ public class CompanyClassifier {
     private void loadCompanyMap() {
         companyKeywords = new HashMap<String, Set<String>>();
 
+        // Load the map defined in resources
         InputStream input = this.getClass().getResourceAsStream("/company_map.json");
         JsonReader reader = new JsonReader(new InputStreamReader(input));
 
-        // Instantiating these every time may take a while...
+        // The object is just a dict so load it into an Object
         JsonParser parser = new JsonParser();
-
         JsonObject elem =  parser.parse(reader).getAsJsonObject();
 
         for (Map.Entry<String, JsonElement> entry : elem.entrySet()) {
+
             String companyName = entry.getKey();
             companyKeywords.put(companyName, new HashSet<String>());
 
-            for (JsonElement comp : entry.getValue().getAsJsonArray()) {
-                companyKeywords.get(companyName).add(comp.getAsString());
-            }
+            // We are storing the lower case of every string match for consistency
+            for (JsonElement comp : entry.getValue().getAsJsonArray())
+                companyKeywords.get(companyName).add(comp.getAsString().toLowerCase());
+
         }
     }
 
@@ -48,10 +50,13 @@ public class CompanyClassifier {
      */
     public String getCompanyMentioned(Status tweet) {
 
+        // We always match lower case strings
+        String tweetText = tweet.toString().toLowerCase();
+
         for (String company : companyKeywords.keySet()) {
             for (String keyword : companyKeywords.get(company)) {
 
-                if (tweet.getText().contains(keyword))
+                if (tweetText.contains(keyword))
                     return company;
 
             }
