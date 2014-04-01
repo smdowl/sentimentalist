38d3f60839a3df80a5aaa0a655388c07f312eaa0
Shaun Dowling <shaun@duedil.com>
2014-04-01 14:02:03 +0100

    Fixed logs path error.

--- start-job.sh
+++ start-job.sh
@@ -1,6 +1,8 @@
+#!/bin/sh
+
 jobname=$1
 mainclass=$2
 
 sh src/main/scripts/gen_steps.sh $jobname $mainclass
 
-elastic-mapreduce --create --name $jobname --log-uri s3://sentiment/logs/$USER --json src/main/jobflows/$jobname.json 
+elastic-mapreduce --create --name $jobname --log-uri s3://sentimentalist/logs/$USER --json src/main/jobflows/$jobname.json
