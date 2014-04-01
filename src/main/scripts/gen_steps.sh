2ca8a0157da431d9d554838cc3df98667b323cdf
Shaun Dowling <shaun@duedil.com>
2014-04-01 13:59:50 +0100

    Added job flow creation code. Gitignore any created job flows.

---
+++ src/main/scripts/gen_steps.sh
@@ -0,0 +1,29 @@
+# gen_steps.sh
+# Create the json file that is requried for elastic-mapreduce to run. 
+# You pass what you would like to call the job and the path of the main class
+# after com.whereismydot.
+#
+# Usage:
+#  src/main/scripts/gen_steps.sh test-job processing.CompanyStats
+
+jobname=$1
+mainclass=$2
+
+mkdir -p src/main/jobflows
+
+echo "[ 
+      { 
+         \"Name\": \"$jobname\", 
+         \"ActionOnFailure\": \"CONTINUE\", 
+         \"HadoopJarStep\": 
+         { 
+            \"MainClass\": \"com.whereismydot.$mainclass\", 
+            \"Jar\": \"s3://sentimentalist/jars/$USER/Sentimentalist-1.0-SNAPSHOT.jar\", 
+            \"Args\": 
+            [ 
+               \"s3://sentimentalist/test-data/mini_1.json\", 
+               \"s3://sentimentalist/output/$USER/test\"
+            ] 
+         } 
+      } 
+]" > src/main/jobflows/$jobname.json
