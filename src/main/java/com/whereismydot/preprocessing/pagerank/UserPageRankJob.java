f7b505a3b354f7d0ccbc283fef5549fa89fba398
Shaun Dowling <shaun@duedil.com>
2014-04-08 21:36:55 +0100

    Update to use Text userIds.

--- src/main/java/com/whereismydot/preprocessing/pagerank/UserPageRankJob.java
+++ src/main/java/com/whereismydot/preprocessing/pagerank/UserPageRankJob.java
@@ -12,39 +12,35 @@ import java.util.LinkedList;
 import java.util.List;
 
 public class UserPageRankJob extends MapReduceBase implements
-        Mapper<LongWritable, Text, LongWritable, Text>,
-        Reducer<LongWritable, Text, LongWritable, Text> {
+        Mapper<LongWritable, Text, Text, Text>,
+        Reducer<Text, Text, Text, Text> {
 
     private Gson gson = new Gson();
 
     @Override
-    public void map(LongWritable key, Text value, OutputCollector<LongWritable, Text> out, Reporter reporter)
+    public void map(LongWritable key, Text value, OutputCollector<Text, Text> out, Reporter reporter)
             throws IOException {
 
         String[] comps = value.toString().split("\t");
-        Long userId = Long.parseLong(comps[0]);
+        String userId = comps[0];
         String json = comps[1];
 
-        // For whatever reason this parses the input as doubles
-        List<Double> doubleList = new LinkedList<>();
-        doubleList = (List<Double>) gson.fromJson(json, doubleList.getClass());
+        List<String> adjacencyList = new LinkedList<>();
+        adjacencyList = (List<String>) gson.fromJson(json, adjacencyList.getClass());
 
-        List<Long> adjacenyList = new LinkedList<>();
-        for (Double d : doubleList)
-            adjacenyList.add(d.longValue());
+        Double p = 1.0 / adjacencyList.size();
 
-        Double p = 1.0 / adjacenyList.size();
+        out.collect(new Text(userId), new Text(json));
 
-        out.collect(new LongWritable(userId), new Text(json));
-
-        for (Long otherId : adjacenyList) {
-            out.collect(new LongWritable(otherId), new Text(p.toString()));
+        for (String otherId : adjacencyList) {
+            out.collect(new Text(otherId), new Text(p.toString()));
         }
     }
 
     @Override
-    public void reduce(LongWritable key, Iterator<Text> values, OutputCollector<LongWritable, Text> out,
+    public void reduce(Text key, Iterator<Text> values, OutputCollector<Text, Text> out,
                        Reporter reporter) throws IOException {
+
         while (values.hasNext())
             out.collect(key, values.next());
     }
@@ -55,7 +51,7 @@ public class UserPageRankJob extends MapReduceBase implements
         job.setMapperClass(UserPageRankJob.class);
         job.setReducerClass(UserPageRankJob.class);
 
-        job.setMapOutputKeyClass(LongWritable.class);
+        job.setMapOutputKeyClass(Text.class);
         job.setMapOutputValueClass(Text.class);
 
         job.setInputFormat(TextInputFormat.class);
