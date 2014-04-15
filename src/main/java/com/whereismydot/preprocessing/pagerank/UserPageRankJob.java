00ee1be466ed7ab67f03264af9f943eb5969d50d
Shaun Dowling <shaunmdowling@googlemail.com>
2014-04-15 21:42:07 +0100

    Set up for iterative page rank. Added counters for lost mass.

--- src/main/java/com/whereismydot/preprocessing/pagerank/UserPageRankJob.java
+++ src/main/java/com/whereismydot/preprocessing/pagerank/UserPageRankJob.java
@@ -1,6 +1,7 @@
 package com.whereismydot.preprocessing.pagerank;
 
 import com.google.gson.Gson;
+import org.apache.hadoop.fs.FileSystem;
 import org.apache.hadoop.fs.Path;
 import org.apache.hadoop.io.Text;
 import org.apache.hadoop.mapred.*;
@@ -14,6 +15,18 @@ public class UserPageRankJob extends MapReduceBase implements
 
     private Gson gson = new Gson();
 
+    public static enum Counter {
+        USERS,
+        LOST_MASS
+    }
+
+    private static final int MAX_IT = 20;
+    private static final double counterScale = 1e6;
+
+    @Override
+    public void configure(JobConf conf) {
+    }
+
     @Override
     public void map(Text userId, Text value, OutputCollector<Text, Text> out, Reporter reporter)
             throws IOException {
@@ -21,13 +34,22 @@ public class UserPageRankJob extends MapReduceBase implements
         Map<String, Object> node = parseNode(value.toString());
 
         List<String> adjacencyList = (List<String>) node.get("adjacency");
-        Double p = 1.0 / adjacencyList.size();
+        Double p = getPageRank(node) / adjacencyList.size();
 
         out.collect(userId, value);
 
         for (String otherId : adjacencyList) {
             out.collect(new Text(otherId), new Text(p.toString()));
         }
+
+        if (adjacencyList.size() == 0) {
+            long lostMass = (long) (getPageRank(node) * counterScale);
+            reporter.getCounter(Counter.LOST_MASS).increment(lostMass);
+        }
+    }
+
+    private double getPageRank(Map<String, Object> node) {
+        return (double) node.get("page_rank");
     }
 
     @Override
@@ -52,8 +74,9 @@ public class UserPageRankJob extends MapReduceBase implements
         }
 
         node.put("page_rank", sum);
-
         out.collect(key, new Text(gson.toJson(node)));
+
+        reporter.getCounter(Counter.USERS).increment(1);
     }
 
     private boolean isNode(String json) {
@@ -74,8 +97,7 @@ public class UserPageRankJob extends MapReduceBase implements
         return node;
     }
 
-    public static void main(String[] args) throws IOException {
-
+    private static JobConf getJobConf() {
         JobConf job = new JobConf(UserPageRankJob.class);
         job.setMapperClass(UserPageRankJob.class);
         job.setReducerClass(UserPageRankJob.class);
@@ -86,9 +108,39 @@ public class UserPageRankJob extends MapReduceBase implements
         job.setInputFormat(KeyValueTextInputFormat.class);
         job.setOutputFormat(TextOutputFormat.class);
 
-        FileInputFormat.setInputPaths(job, new Path(args[0]));
-        FileOutputFormat.setOutputPath(job, new Path(args[1]));
+        return job;
+    }
+
+    public static void main(String[] args) throws IOException {
+
+        int iteration = 0;
+
+        JobConf job = getJobConf();
+
+        Path basePath = new Path(args[1]);
+        FileSystem fs = FileSystem.get(job);
+
+        fs.delete(basePath, true);
+        fs.mkdirs(basePath);
+
+        Path inputPath = new Path(args[0]);
+        Path outputPath = new Path(basePath, "iteration" + iteration);
+        FileInputFormat.setInputPaths(job, inputPath);
+        FileOutputFormat.setOutputPath(job, outputPath);
 
         JobClient.runJob(job);
+
+        // Perform iterations up to the maximum number
+        while (iteration < MAX_IT) {
+            iteration++;
+
+            job = getJobConf();
+
+            FileInputFormat.setInputPaths(job, outputPath);
+            outputPath = new Path(basePath, "iteration" + iteration);
+            FileOutputFormat.setOutputPath(job, outputPath);
+
+            JobClient.runJob(job);
+        }
     }
 }
