3fd4ebe8f91fa61b8b06ab7dfc1770d4acbb8dc5
Shaun Dowling <shaunmdowling@googlemail.com>
2014-04-19 20:11:07 +0100

    Split utils into separate class.

--- src/main/java/com/whereismydot/preprocessing/pagerank/UserPageRankJob.java
+++ src/main/java/com/whereismydot/preprocessing/pagerank/UserPageRankJob.java
@@ -25,28 +25,6 @@ enum Counter {
     HANGING_NODE
 }
 
-class Utils {
-    static private Gson gson = new Gson();
-    static final double COUNTER_SCALE = 1e6;
-
-    static final String LOST_MASS = "LostMass";
-    static final String USER_COUNT = "UserCount";
-
-    static Map<String, Object> parseNode(String json) {
-        HashMap<String, Object> node = new HashMap<>();
-        node = gson.fromJson(json, node.getClass());
-        return node;
-    }
-
-    static long convertMass(double mass) {
-        return (long) (mass * COUNTER_SCALE);
-    }
-
-    static double convertMass(long mass) {
-        return (1.0 * mass) / COUNTER_SCALE;
-    }
-}
-
 class UserPageRankMapper extends Mapper<Text, Text, Text, Text> {
 
     @Override
